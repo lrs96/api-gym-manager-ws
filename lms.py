03cd4f94408fe909146f2ab93fc21d5c47523821
Luan Rodrigues <luanlrs96@gmail.com>
2020-05-12 21:44:30 -0300

    Mudança de disciplinas para alunos

--- lms.py
+++ lms.py
@@ -6,50 +6,50 @@ import random
 
 app = Flask(__name__)
 
-disciplinas = [{"id": e, "nome": "Disciplina "+str(e), "ementa":"Ementa "+str(e), "foto":"https://cdn.pixabay.com/photo/2018/01/18/20/42/pencil-3091204_1280.jpg", "professor": "Professor Disciplina "+str(e)} for e in range(1,11)]   
+alunos = [{"id": e, "nome": "{}".format(str(e)), "cpf": e, "foto":"https://images.pexels.com/photos/3214950/pexels-photo-3214950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", "idade": e } for e in range(1,11)]   
 
-@app.route("/disciplinas", methods=['GET'])
+@app.route("/alunos", methods=['GET'])
 def get():
-    return jsonify(disciplinas)
+    return jsonify(alunos)
 
-@app.route("/disciplinas/<int:id>", methods=['GET'])
+@app.route("/alunos/<int:id>", methods=['GET'])
 def get_one(id):
-    filtro = [e for e in disciplinas if e["id"] == id]
+    filtro = [e for e in alunos if e["id"] == id]
     if filtro:
         return jsonify(filtro[0])
     else:
         return jsonify({})
 
-@app.route("/disciplinas", methods=['POST'])
+@app.route("/alunos", methods=['POST'])
 def post():
-    global disciplinas
+    global alunos
     try:
         content = request.get_json()
 
         # gerar id
-        ids = [e["id"] for e in disciplinas]
+        ids = [e["id"] for e in alunos]
         if ids:
             nid = max(ids) + 1
         else:
             nid = 1
         content["id"] = nid
-        disciplinas.append(content)
+        alunos.append(content)
         return jsonify({"status":"OK", "msg":"disciplina adicionada com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
-@app.route("/disciplinas/<int:id>", methods=['DELETE'])
+@app.route("/alunos/<int:id>", methods=['DELETE'])
 def delete(id):
-    global disciplinas
+    global alunos
     try:
-        disciplinas = [e for e in disciplinas if e["id"] != id]
+        alunos = [e for e in alunos if e["id"] != id]
         return jsonify({"status":"OK", "msg":"disciplina removida com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
 @app.route("/push/<string:key>/<string:token>", methods=['GET'])
 def push(key, token):
-	d = random.choice(disciplinas)
+	d = random.choice(alunos)
 	data = {
 		"to": token,
 		"notification" : {
